e215a86db74ac0095b6243622222ba40f720d52c
Kseniya Nifontova <knifontova@gmail.com>
2023-12-10 14:55:25 +0100

    first commit

---
+++ map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3NuaWZvbnRvdmEiLCJhIjoiY2w3MHJvYWNhMGhieDNwcGdiN2luamJ2NSJ9.P6p3srRpmB1MHge4ulAQfw';
+const map = new mapboxgl.Map({
+    container: 'emap', // container ID
+    style: 'mapbox://styles/ksnifontova/clpzhwyfu003l01pl3ss9ap2c', // style URL
+    center: [11.573617, 48.155559], // starting position [lng, lat]
+    zoom: 10.5, // starting zoom
+    projection: 'albers' // projection
+});
